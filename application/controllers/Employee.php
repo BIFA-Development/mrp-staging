d8595e6d5079f06dd4dba186c2b5760d2e6128be
Zakario Lesrahmanto <andrio.zaboy@gmail.com>
2025-07-30 15:58:32 +0700

    fix: test spasi

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -723,7 +723,7 @@ class Employee extends MY_Controller
 
             $data = array();
             $no   = $_POST['start'];
-            
+             
 
             foreach ($entities as $row){
                 $no++;
